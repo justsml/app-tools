00955baa8d64ae30cdc6d74fcc35dc0bf7b7f5dc
justsml <dan@hillsoft.com>
2014-12-13 21:47:24 -0700

    Cache Updates, reworking lib and tests

--- cache/index.js
+++ cache/index.js
@@ -1,61 +1,70 @@
 "use strict";
 var MongoClient = require('mongodb').MongoClient,
-		ObjectID = require('mongodb').ObjectID;
+		ObjectID = require('mongodb').ObjectID
 /*
 
 Example:
-var Cache = Require('app/cache')
+var Cache = require('app/cache')
 var cache = new Cache(req.app.db, 'propertydetailscache')
 cache.setItem('')
 */
 var Cache = module.exports = function(db, collectionName) {
-	this.collectionName = collectionName || 'cache'
-	this.db = db
+	var self = this;
+	self.collectionName = collectionName || 'cache'
+	self.db = db || null;
+	checkConnection(self.db, function(err, db) {
+		if ( err ) { throw err } // Should fail early if we don't have a db
+		if ( !db ) { throw new Error('Failed to connect to database') } // Should fail early if we don't have a db
+		self.db = db
+	})
 }
 Cache.prototype.getItem = function(key, callback) {
-	checkConnection(this.db, function (db) {
-		if ( db && db.collection ) {
-			db.collection(this.collectionName, function _getColl(err, c) {
-				if ( err ) { return callback(err, c) }
-				c.find({_id: key}, function _find(err, data) {
-					// console.warn('FIND RESULTS', err, data)
-					callback(err, data)
-				})
+	var self = this;
+	var db = self.db;
+	if ( db && db.collection ) {
+		db.collection(self.collectionName, function _getColl(err, c) {
+			if ( err ) { return callback(err, c) }
+			c.find({_id: key}, function _find(err, data) {
+				// console.warn('FIND RESULTS', err, data)
+				callback(err, data)
 			})
-		} else {
-			return callback(new Error('Couldn\'t connect to db'))
-		}
-	})
+		})
+	} else {
+		return callback(new Error('Couldn\'t connect to db'))
+	}
 }
 Cache.prototype.setItem = function(key, value, callback) {
-	checkConnection(this.db, function (db) {
-		if ( db && db.collection ) {
-			db.collection(this.collectionName, function _getColl(err, c) {
-				if ( err ) { return callback(err, c) }
-				// check if we need to remove instead of upsert
-				if ( ! value ) {
-					c.remove({_id: key}, callback)
-				} else {
-					value._id = key
-					// WARNING: Upsert w/ no saftey
-					c.update({_id: key}, value, {upsert:true, w: 0}, callback)
-				}
-			})
-		} else {
-			return callback(new Error('Couldn\'t connect to db'))
-		}
-	})
+	var self = this;
+	var db = self.db;
+	if ( db && db.collection ) {
+		db.collection(self.collectionName, function _getColl(err, c) {
+			if ( err ) { return callback(err, c) }
+			// check if we need to remove instead of upsert
+			if ( ! value ) {
+				c.remove({_id: key}, callback)
+			} else {
+				value._id = key
+				// WARNING: Upsert w/ no saftey
+				c.update({_id: key}, value, {upsert:true, w: 0}, callback)
+			}
+		})
+	} else {
+		return callback(new Error('Couldn\'t connect to db'))
+	}
 }
 function checkConnection(db, callback) {
 	if ( db && db.collection ) {
-		callback(db)
+		callback(null, db)
 	} else {
-		MongoClient.connect('mongodb://127.0.0.1:27017/' (typeof(db) === 'string' ? db : 'cache'), function(err, db) {
+		var mongoHost = process.env.MONGOCACHE_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGO_URL || 'localhost:27017/cache'
+		mongoHost = mongoHost.split('/')[0]
+		mongoHost = mongoHost + '/' + (typeof(db) === 'string' ? db : 'cache');
+		MongoClient.connect('mongodb://' + mongoHost, function(err, db) {
 			if(err) {
 				console.error('Mongo Failed Connection', err)
-				return callback(null);
+				return callback(err, null);
 			}
-			callback(db)
+			callback(err, db)
 		});
 	}
 }
--- cache/test/test.js
+++ cache/test/test.js
@@ -1,10 +1,11 @@
 "use strict";
 
-var Cache = require('app/cache')
-var cache = new Cache(null, 'propertydetailscache')
+var Cache = require('../')
+// var cache = new Cache(null, 'cache')
+var cache = new Cache()
 var assert = require('assert');
 
-describe('Check Cache Instance\'s Interface', function(){
+describe('Check Cache Features', function() {
   it('Check methods', function(done){
 		if (! cache.setItem) {
 			throw new Error('setItem not found')
@@ -12,32 +13,29 @@ describe('Check Cache Instance\'s Interface', function(){
 		if (! cache.getItem) {
 			throw new Error('getItem not found')
 		}
-		done();
+		done()
 	});
-});
-describe('SET foo', function(){
-  it('setItem', function(done){
+
+  it('Save Item', function(done){
 		cache.setItem('foo', {foo: 'bar'}, function(err, result) {
 			assert.equal(null, err)
 		})
-		done();
+		done()
 	})
-})
-describe('GET foo', function(){
-  it('Check Saved Item', function(done){
+
+  it('Get Saved Item', function(done){
 		cache.getItem('foo', function(err, result) {
 			assert.equal(null, err)
 			assert.equal(result.foo, 'bar')
 		})
 		done()
 	})
-})
-describe('REMOVE foo', function(){
+
   it('Delete Saved Item', function(done){
 		cache.setItem('foo', null, function(err, result) {
 			assert.equal(null, err)
 			// throw new Error(result)
-			assert.equal(result.foo, 'bar')
+			// assert.equal(result.foo, 'bar')
 		})
 		done()
 	})
