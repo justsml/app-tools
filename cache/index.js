3e28ddbc8f318c5d0f71c51ade07bfc9c41597a2
justsml <dan@hillsoft.com>
2014-12-14 19:16:03 -0700

    Working Tests - MongoDB must now be passed into the Cache instances ctor()

--- cache/index.js
+++ cache/index.js
@@ -1,8 +1,4 @@
 "use strict";
-var MongoClient = require('mongodb').MongoClient,
-		ObjectID = require('mongodb').ObjectID,
-		EventEmitter = require('events').EventEmitter,
-		util = require('util')
 /*
 
 Example:
@@ -12,84 +8,53 @@ cache.setItem('')
 */
 var Cache = module.exports = function(db, collectionName) {
 	var self = this;
-	self.ready = false;
-	// EventEmitter.call(self);
 	self.collectionName = collectionName || 'cache'
 	self.db = db || null;
-	// self.checkConnection(self.db, function(err, db) {
-	// 	if ( err ) { throw err } // Should fail early if we don't have a db
-	// 	if ( !db ) { throw new Error('Failed to connect to database') } // Should fail early if we don't have a db
-	// })
 }
-// util.inherits(Cache, EventEmitter);
-
+Cache.prototype.checkIndexes = function(collection) {
+	var self = this;
+	if ( ! self.indexChecked ) {
+		collection.ensureIndex( { "createdDate": 1 }, { expireAfterSeconds: (2 * 24 * 60 * 60) } )
+	}
+	self.indexChecked = true
+}
 Cache.prototype.getItem = function(key, callback) {
 	var self = this;
-	self.checkConnection(self.db, function(err, db) {
-		if ( err ) { throw err } // Should fail early if we don't have a db
-		if ( !db ) { throw new Error('Failed to connect to database') } // Should fail early if we don't 
-		if ( db && db.collection ) {
-			db.collection(self.collectionName, function _getColl(err, c) {
-				if ( !c ) { return callback(err, new Error('Error getting collection')) }
-				c.find({_id: key}).toArray(function(err, items) {
-					callback(err, items && items[0])
-				})
+	var db = self.db
+	if ( !db ) { return callback(new Error('Failed to connect to database')) } // Should fail early if we don't 
+	if ( db && db.collection ) {
+		db.collection(self.collectionName, function _getColl(err, c) {
+			self.checkIndexes(c)
+			if ( !c ) { return callback(err, new Error('Error getting collection')) }
+			c.find({_id: key}).toArray(function(err, items) {
+				callback(err, items && items[0] && items[0].data)
 			})
-		} else {
-			return callback(new Error('Couldn\'t connect to db'))
-		}
-	})
+		})
+	} else {
+		return callback(new Error('Couldn\'t connect to db'))
+	}
 }
 Cache.prototype.setItem = function(key, value, callback) {
 	var self = this;
-	self.checkConnection(self.db, function(err, db) {
-		if ( err ) { throw err } // Should fail early if we don't have a db
-		if ( !db ) { throw new Error('Failed to connect to database') } // Should fail early if we don't 
-		if ( db && db.collection ) {
-			db.collection(self.collectionName, function _getColl(err, c) {
-				if ( err ) { return callback(err, new Error('Error getting collection')) }
-				// check if we need to remove instead of upsert
-				if ( ! value ) {
-					c.remove({_id: key}, {w: 0}, callback)
-				} else {
-					value._id = key
-					// WARNING: Upsert w/ no saftey
-					c.update({_id: key}, value, {upsert:true, w: 0}, callback)
-				}
-			})
-		} else {
-			return callback(new Error('Couldn\'t connect to db'))
-		}
-	})
-}
-// Add this as a member method for now... should be private
-Cache.prototype.checkConnection = function(db, callback) {
-	var self = this
+	var db = self.db
+	if ( !db ) { return callback(new Error('Failed to connect to database')) } // Should fail early if we don't 
 	if ( db && db.collection ) {
-		callback(null, db)
-	} else {
-		var mongoHost = process.env.MONGOCACHE_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGO_URL || 'localhost:27017/cache'
-		mongoHost = mongoHost.split('/')[0]
-		mongoHost = mongoHost + '/' + (typeof(db) === 'string' ? db : 'cache');
-		// console.log('Connecting to ', mongoHost)
-		MongoClient.connect('mongodb://' + mongoHost, {w: 0}, function(err, db) {
-			if(err) {
-				self.error = err
-				console.error('Mongo Failed Connection', err)
-				return callback(err, null);
-			}
-			self.connecting = false
-			self.ready = true
-			self.db = db
-			if ( ! db.db.isInitializing ) {
-				callback(err, db)
+		db.collection(self.collectionName, function _getColl(err, c) {
+			self.checkIndexes(c)
+			if ( err ) { return callback(err, new Error('Error getting collection')) }
+			// check if we need to remove instead of upsert
+			if ( ! value ) {
+				c.remove({_id: key}, {w: 0}, callback)
 			} else {
-				setTimeout(function() {
-					callback(err, db)
-				}, 200)
+				var fieldsToSave = { '_id': key, 'data': value, 'createdDate': new Date() }
+				// WARNING: Upsert w/ no saftey
+				c.update({_id: key}, fieldsToSave, {upsert:true, w: 0}, callback)
 			}
 		})
+	} else {
+		return callback(new Error('Couldn\'t connect to db'))
 	}
 
 }
+
 
